4028a4382c9c468a21aeb64730c6c8f0da520db4
OpTudor <133231902+OpTudor@users.noreply.github.com>
2023-08-02 00:54:21 +0300

    Finished game logic

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Player = (sign) => {
+function Player(sign) {
   this.sign = sign;
   const getSign = () => {
     return sign;
   };
 
   return { getSign };
-};
+}
 
 const gameBoard = (() => {
   const board = ["", "", "", "", "", "", "", "", ""];
@@ -28,15 +28,17 @@ const gameBoard = (() => {
 })();
 
 const displayController = (() => {
-  const fields = document.querySelectorAll("field");
+  const fields = document.querySelectorAll(".field");
   fields.forEach((field) => {
     field.addEventListener("click", (e) => {
-      gameBoard.setField(e.target.dataset.index, "x");
-      updateGameBoard();
+      if (e.target.textContent === "") {
+        gameController.playRound(e.target.dataset.index);
+        updateGameBoard();
+      }
     });
 
     const updateGameBoard = () => {
-      for (let i = 0; i < field.length; i++) {
+      for (let i = 0; i < fields.length; i++) {
         fields[i].textContent = gameBoard.getField(i);
       }
     };
@@ -47,4 +49,52 @@ const gameController = (() => {
   const playerX = Player("X");
   const player0 = Player("0");
   let moves = 0;
+
+  const playRound = (index) => {
+    console.log(moves);
+
+    gameBoard.setField(index, getCurrentPlayer());
+    moves++;
+    const winner = checkWinner();
+    if (winner) {
+      alert(`${winner} wins!`);
+      gameBoard.resetBoard();
+      moves = 0;
+    }
+    if (moves === 9) {
+      alert("Draw");
+      gameBoard.resetBoard();
+      moves = 0;
+    }
+  };
+
+  const getCurrentPlayer = () => {
+    return moves % 2 === 0 ? playerX.getSign() : player0.getSign();
+  };
+
+  const checkWinner = () => {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < winningConditions.length; i++) {
+      const [a, b, c] = winningConditions[i];
+      const fieldA = gameBoard.getField(a);
+      const fieldB = gameBoard.getField(b);
+      const fieldC = gameBoard.getField(c);
+
+      if (fieldA && fieldA === fieldB && fieldA === fieldC) {
+        return fieldA;
+      }
+    }
+    return null;
+  };
+
+  return { playRound };
 })();
